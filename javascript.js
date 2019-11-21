6efec773d29ef3d24e02d0366a3543081f35c0a1
Gabby Jones <jones.gm90@gmail.com>
2019-11-21 10:20:50 -0500

    added some js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-var passwordChar=["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"];
+var charArray=["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"];
 
 var userInput= prompt("Choose a password length between 8 and 20.");
+
+function generatePassword(){
+for (var i = 0; n = charArray.length;  i <= (userInput); i++; {
+    retVal += charArray.charAt(Math.random() * n);
+}
+}
 
