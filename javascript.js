bebef2801ca68a83dd38044795842fad5c811906
Gabby Jones <jones.gm90@gmail.com>
2019-11-21 11:27:10 -0500

    added prompt to choose pw length

--- javascript.js
+++ javascript.js
@@ -2,9 +2,13 @@ var charArray=["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@
 
 var userInput= prompt("Choose a password length between 8 and 20.");
 
+var password= 
 function generatePassword(){
 for (var i = 0; n = charArray.length;  i <= (userInput); i++; {
     retVal += charArray.charAt(Math.random() * n);
 }
 }
 
+function copyStringToClipboard(str) {
+
+}
