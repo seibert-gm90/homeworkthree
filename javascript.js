ac76c96f66a25ed63d139272e7df7442ded132b7
Gabby Jones <jones.gm90@gmail.com>
2019-11-22 21:17:52 -0500

    added onclick event to copy generated password to clipboard and added function and alert for user.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 var randomCharacters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
 var charArray = randomCharacters.split('')
 var n = charArray.length
-var passwordLength= prompt("Choose a password length between 8 and 20.");
+var passwordLength= prompt("Choose a password length between 8 and 20.")
+
 
 function generatePassword(){
     var passwordInput = document.querySelector("#inputPassword5")
@@ -11,13 +12,15 @@ function generatePassword(){
         var char = charArray[Math.floor(Math.random() * n)];
         generatedPassword = generatedPassword + char
     }
-
     passwordInput.value = generatedPassword
-
 }
 
 
 
-function copyStringToClipboard(str) {
+function copyStringToClipboard() {
+    var copyText = document.querySelector("#inputPassword5");
 
+    copyText.select("#inputPassword5");
+    document.execCommand("copy");
+    alert("Copied the text." + copyText.value)
 }
