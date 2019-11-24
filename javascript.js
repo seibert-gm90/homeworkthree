3d3b4ef518abd3f5a7fa9465186e5a3600267ecf
Gabby Jones <jones.gm90@gmail.com>
2019-11-23 22:20:36 -0500

    adding checkboxes and functions to checkboxes.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,49 @@
-var randomCharacters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-var charArray = randomCharacters.split('')
-var n = charArray.length
-var passwordLength= prompt("Choose a password length between 8 and 20.")
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase= "abcdefghijklmnopqrstuvwxyz"
+var num = "0123456789"
+var specialChar= "!@#$%^&*()"
 
+// var passwordLength= prompt("Choose a password length between 8 and 20.")
+var userPwEl = document.querySelector("#userPwInput")
+var upperCaseCheckbox = document.querySelector("#uppercase")
+var lowerCaseCheckbox = document.querySelector("#lowercase")
+var numCheckbox = document.querySelector("#number")
+var specialCharCheckbox = document.querySelector("#specialCharacter")
 
 function generatePassword(){
     var passwordInput = document.querySelector("#inputPassword5")
     var generatedPassword = ""
+    var passwordLength = userPwEl.value
+
+    var upperChecked = upperCaseCheckbox.checked;
+    var lowerChecked = lowerCaseCheckbox.checked;
+    var numberChecked = numCheckbox.checked;
+    var specialCharacterChecked = specialCharCheckbox.checked;
+    var charArray; 
+    if (!upperChecked && !lowerChecked && !numberChecked && !specialCharacterChecked ){
+        alert ("Please check a box!")
+        return;
+    }
+    if (upperChecked){
+        charArray += upperCase
+    }
+
+    if (lowerChecked){
+        charArray += lowerCase
+
+    }
+
+    if (numberChecked){
+        charArray += num
+
+    }
+
+    if (specialCharacterChecked){
+        charArray += specialChar
+    }
 
     for (var i = 0; i < (passwordLength); i++) {
-        var char = charArray[Math.floor(Math.random() * n)];
+        var char = charArray[Math.floor(Math.random() * charArray.length)];
         generatedPassword = generatedPassword + char
     }
     passwordInput.value = generatedPassword
